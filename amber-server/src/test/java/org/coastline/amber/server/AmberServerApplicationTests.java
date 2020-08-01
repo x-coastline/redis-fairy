cc8f59a45f83b0df289c38864beeeab2f50f110c
zouhuajian <zouhuajianclever@gmail.com>
2020-08-01 20:33:48 +0800

    amber server

---
+++ amber-server/src/test/java/org/coastline/amber/server/AmberServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coastline.amber.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmberServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
