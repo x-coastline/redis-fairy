b94845b8aaa1c19a4335d743c48e8649a72bdbd5
zouhuajian <zouhuajianclever@gmail.com>
2020-09-08 19:06:00 +0800

    RegistryService

---
+++ amber-server/src/main/java/org/coastline/amber/server/registry/RegistryService.java
@@ -0,0 +1,15 @@
+package org.coastline.amber.server.registry;
+
+/**
+ * @author Jay.H.Zou
+ * @date 2020/9/4
+ */
+public interface RegistryService {
+
+    /**
+     * 服务注册
+     */
+    void register();
+
+
+}
